11ad247a3563827987462abe801db322a4e30494
h-ihninger <97694432+h-ihninger@users.noreply.github.com>
2022-02-07 19:54:53 +0100

    upt Docu

--- src/sequencer/src/main.c
+++ src/sequencer/src/main.c
@@ -15,8 +15,6 @@ void app_main(void)
     for (int i = 0; i < WAVEFORM_CNT; i++)
     {
         printf("%f\n", get_wavetable(SQUARE_WT)[i]);
-
-        // printf("%c", (i % 8) ? (' ') : ('\n'));
     }
     return;
 #endif
@@ -24,6 +22,8 @@ void app_main(void)
     while (1)
     {   
         i2s_write_sample_24ch2(get_wavetable(SINE_WT));
+
+        
         // vTaskDelay(10 / portTICK_RATE_MS);
     }
 }
