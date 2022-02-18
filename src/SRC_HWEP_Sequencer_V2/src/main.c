af237d68bf128f2382eb3c74fd04731e40188c64
s-grundner <simon.grundner04@htl-salzburg.ac.at>
2022-02-18 13:34:57 +0100

    mcp interrupt upd

--- src/SRC_HWEP_Sequencer_V2/src/main.c
+++ src/SRC_HWEP_Sequencer_V2/src/main.c
@@ -1,11 +1,54 @@
 #include "config.h"
-#include "driver/adc.h"
-#include "esp_adc_cal.h"
-#include "esp_intr_alloc.h"
+
+#define INTR_MASK (1ULL << GPIO_NUM_32)
+
+volatile mcp23s08_handle_t mcp_handle = NULL;
+
+static QueueHandle_t gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void *arg)
+{
+    uint32_t gpio_num = (uint32_t)arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void *arg)
+{
+    uint32_t io_num;
+    for (;;)
+    {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY))
+        {
+            uint8_t data = 0;
+            esp_err_t err = mcp23s08_read(mcp_handle, HW_ADR_0, GPIO_R, &data);
+            ESP_ERROR_CHECK(err);
+            printf("%x\n", data);
+            printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+    }
+}
 
 void app_main(void)
 {
     esp_err_t err;
+
+    // ------------------------------------------------------------
+    // Interrupts
+    // ------------------------------------------------------------
+
+    gpio_config_t intr_cfg = {
+        .intr_type = GPIO_INTR_POSEDGE,
+        .pull_down_en = 1,
+        .pull_up_en = 0,
+        .mode = GPIO_MODE_INPUT,
+        .pin_bit_mask = 1ULL << GPIO_NUM_32,
+    };
+    gpio_config(&intr_cfg);
+    gpio_set_intr_type(GPIO_NUM_32, GPIO_INTR_POSEDGE);
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(GPIO_NUM_32, gpio_isr_handler, (void *)GPIO_NUM_32);
 
     // ------------------------------------------------------------
     // SPI Bus
@@ -14,13 +57,12 @@ void app_main(void)
     err = spi_bus_initialize(VSPI_HOST, &vspi_pin_cfg, 0);
     ESP_ERROR_CHECK(err);
 
-    mcp23s08_handle_t mcp_handle = NULL;
     mcp23s08_config_t mcp_cfg = {
         .host = VSPI_HOST,
         .miso_io = VSPIQ,
         .mosi_io = VSPID,
         .cs_io = CS_MCP23S08,
-        .intr_io = MCP23S08_INTR,
+        .intr_io = -1,
     };
 
     err = mcp23s08_init(&mcp_handle, &mcp_cfg);
@@ -28,6 +70,11 @@ void app_main(void)
 
     err = mcp23s08_write(mcp_handle, HW_ADR_0, IODIR, 0xff);
     ESP_ERROR_CHECK(err);
+
+    // err = mcp23s08_write(mcp_handle, HW_ADR_0, , 0xff);
+    // ESP_ERROR_CHECK(err);
+
+    // uint8_t data = 0;
 
     // ------------------------------------------------------------
     // ADC
@@ -40,19 +87,11 @@ void app_main(void)
     // Timer
     // ------------------------------------------------------------
 
-    // ------------------------------------------------------------
-    // Interrupts
-    // ------------------------------------------------------------
-
-    esp_intr_alloc(ETS_GPIO_INTR_SOURCE, ESP_INTR_FLAG_EDGE, NULL, NULL, NULL);
-
-    uint8_t *data = (uint8_t *)malloc(sizeof(uint8_t *));
+    // gpio_isr_handler_remove(GPIO_NUM_32);
+    uint8_t data = 0;
 
     while (1)
     {
-        err = mcp23s08_read(mcp_handle, HW_ADR_0, GPIO_R, data);
-        ESP_ERROR_CHECK(err);
-        printf("%d\n", *data);
         vTaskDelay(10);
     }
 }
